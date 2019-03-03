cd4013b753385813cb7b13072cad60dda1fd76e2
Vasu Sheoran <gokusayon@gmail.com>
2019-03-04 01:03:20 +0530

    Modified model to ANN with input shape 100, 11, 1. Applied comp_filter still pending.

--- app/src/main/java/com/bits/har/TensorFlowClassifier.java
+++ app/src/main/java/com/bits/har/TensorFlowClassifier.java
@@ -5,25 +5,24 @@ import android.content.Context;
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 
-public class TensorFlowClassifier {
+class TensorFlowClassifier {
     static {
         System.loadLibrary("tensorflow_inference");
     }
 
     private TensorFlowInferenceInterface inferenceInterface;
-    private static final String MODEL_FILE = "file:///android_asset/tf_model_1.pb";
-//    private static final String MODEL_FILE = "file:///android_asset/frozen_har.pb";
-    private static final String INPUT_NODE = "reshape_1_input_5";
-    private static final String[] OUTPUT_NODES = {"dense_4_5/BiasAdd"};
-    private static final String OUTPUT_NODE = "dense_4_5/BiasAdd";
-    private static final long[] INPUT_SIZE = {1, 10, 3};
+    private static final String MODEL_FILE = "file:///android_asset/cnn/v2/best_model.0.95-1.00-07-0.21.tflite";
+    private static final String INPUT_NODE = "conv2d_1_input_1";
+    private static final String[] OUTPUT_NODES = {"dense_3_1/Softmax"};
+    private static final String OUTPUT_NODE = "dense_3_1/Softmax";
+    private static final long[] INPUT_SIZE = {1, 100, 11, 1};
     private static final int OUTPUT_SIZE = 3;
 
-    public TensorFlowClassifier(final Context context) {
+    TensorFlowClassifier(final Context context) {
         inferenceInterface = new TensorFlowInferenceInterface(context.getAssets(), MODEL_FILE);
     }
 
-    public float[] predictProbabilities(float[] data) {
+    float[] predictProbabilities(float[] data) {
         float[] result = new float[OUTPUT_SIZE];
         inferenceInterface.feed(INPUT_NODE, data, INPUT_SIZE);
         inferenceInterface.run(OUTPUT_NODES);
